85a7d2fae8c18f5bb6e1c8b60ba1debcb59047bc
turab0075 <turabpatel964@gmail.com>
2025-10-18 05:21:09 +0000

    variables

---
+++ java-script/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail ="turabpatel964@gmail.com"
+var accountPassword="12344432"
+accountCity="surat"
+
+accountEmail="hcl123@gmail.com"
+accountPassword="55453533"
+accountCity="mumbai"
+accountState;
+/* if you dont enter value the variable choose value undefined automatic*/ 
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+/*
+perfer not use var
+because of issue in block scope and functional scope
+*/
