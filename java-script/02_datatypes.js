7de4b78c99c28557a24c05c16d3b1247f5a1d7aa
turab0075 <turabpatel964@gmail.com>
2025-10-18 06:09:16 +0000

    datatypes

---
+++ java-script/02_datatypes.js
@@ -0,0 +1,20 @@
+"new strict";/* treat all js code as a newer version */
+
+
+// alert(3+3) we are using node js not browser */
+
+let name = "turab"
+let age = 18
+let inLogged=true
+//number=> 2 to power 53
+//biginit
+//string=>""
+//Boolean=> true/false
+//null=> standalone value
+//undefined=>
+//symbol
+
+
+//object
+console.log(typeof null);//output object
+console.log(typeof undefined);// output undefined
