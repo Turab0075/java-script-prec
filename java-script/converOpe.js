fa7e4192ec034d8b25254dc8a0f1f62f342c9c8c
turab0075 <turabpatel964@gmail.com>
2025-10-20 06:16:40 +0000

    conversation 2

--- java-script/converOpe.js
+++ java-script/converOpe.js
@@ -28,5 +28,42 @@ let stringNumber=String(someNumber)
 
 console.log( typeof stringNumber);
 
+//   *********************OPERATIONS******************///
+
+let value = 5
+let nagValue = -value
+console.log(nagValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);*/
+
+let str1 ="hello"
+let str2 =" turab"
+ let str3 = str1 + str2
+ console.log(str3);
+
+
+ console.log("1"+ 2);
+ console.log(1 + "2");
+ console.log("1"+ 2 + 2);
+ console.log(1 + 2 + "2");
+
+
+ let gameCounter =100
+  gameCounter++
+  console.log(gameCounter);
+  
+
+ 
+ 
+ 
+ 
+
+
+
 
 
